363e1823b8eaba77ccf6cbe6a0adac8a8d0efea6
RishabhShreevas <rishabhshreevas24@gmail.com>
2026-04-09 16:03:20 +0000

    fixed js effects

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,22 +2,26 @@
 
 console.log("Hello, World!");
 
-function randomNumber (min,max){
+function randomNumber(min,max) {
     return Math.floor(Math.random() * (max-min +1)) + min;
 }
+
 let divs = document.querySelectorAll(".grid-container div");
+
 function randomize(){
+    console.log(divs.length, "randomize!")
+
     divs.forEach(function(div){
         let scale = randomNumber(.5, 1.2);
         let translateX = randomNumber(0,50);
         let translateY = randomNumber(0,20);
         let rotate = randomNumber(0,360);
         div.style.transform =
-        scale(${scale})
-        translate(${translateX}%, ${translateY}%)
-        rotate(${rotate}deg);
-    })
-    console.log(divs.length, "randomize!")
+            `scale(${scale})
+            translate(${translateX}%, ${translateY}%)
+            rotate(${rotate}deg)`;
+
+    });
 }
 //randomize();
 document.addEventListener("click", randomize);
